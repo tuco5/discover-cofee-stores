c2d7df80bb3223a53bacd4c5686ce28db3c45797
Tudor <tudor.comparan@gmail.com>
2022-03-31 15:41:31 -0600

    Card component created

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,8 @@ import Head from 'next/head';
 import Image from 'next/image';
 
 import Banner from '../components/banner';
+import Card from '../components/card';
+import coffeeStores from '../data/coffee-stores.json';
 
 import styles from '../styles/Home.module.scss';
 
@@ -14,23 +16,39 @@ export default function Home() {
       <Head>
         <title>Coffee Conoisseur</title>
         <meta
-          name='description'
-          content='Coffee stores finder near by me landing page.'
+          name="description"
+          content="Coffee stores finder near by me landing page."
         />
       </Head>
 
       <main className={styles.main}>
         <Banner
-          buttonText='view stores nearby'
+          buttonText="view stores nearby"
           handleOnClick={bannerBtnHandleOnClick}
         />
+
         <div className={styles.heroImage}>
           <Image
-            src='/static/hero-image.png'
-            alt='hero image'
+            src="/static/hero-image.png"
+            alt="hero image"
             width={700}
             height={400}
           />
+        </div>
+
+        <div className={styles.cardLayout}>
+          {coffeeStores.map(
+            ({ id, name, imgUrl, websiteUrl, ...otherProps }) => (
+              <Card
+                key={id}
+                className={styles.card}
+                name={name}
+                imgUrl={imgUrl}
+                href={`/coffee-store/${id}`}
+                {...otherProps}
+              />
+            )
+          )}
         </div>
       </main>
     </div>
