af65f66e32e9cc15671c84ae72570023ab5bbe04
Tudor <tudor.comparan@gmail.com>
2022-03-31 15:41:31 -0600

    Card component created

---
+++ components/card.jsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import classNames from 'classnames';
+
+import styles from './card.module.scss';
+
+const Card = ({ name, imgUrl, href }) => (
+  <Link href={href}>
+    <a className={styles.cardLink}>
+      <div className={classNames('glass', styles.container)}>
+        <div className={styles.cardHeaderWrapper}>
+          <h2 className={styles.cardHeader}>{name}</h2>
+        </div>
+        <div className={styles.cardImageWrapper}>
+          <Image
+            className={styles.cardImage}
+            src={imgUrl}
+            alt={name}
+            width={260}
+            height={160}
+          />
+        </div>
+      </div>
+    </a>
+  </Link>
+);
+
+export default Card;
