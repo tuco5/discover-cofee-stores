6ff3523605fa31bb02eb5d04a51da890aeac3b3d
Tudor <tudor.comparan@gmail.com>
2022-04-18 12:19:44 -0500

    Section 8 finished

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,11 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import { useEffect } from 'react/cjs/react.production.min';
+import { useContext, useEffect, useState } from 'react';
+import useTrackLocation from '../hooks/use-track-location';
+import { ACTION_TYPES, StoreContext } from '../context/store-context';
 
 import Banner from '../components/banner';
 import Card from '../components/card';
-import useTrackLocation from '../hooks/use-track-location';
 import { fetchCoffeeStores } from '../lib/coffee-stores';
 
 import styles from '../styles/Home.module.scss';
@@ -19,27 +20,39 @@ export async function getStaticProps(context) {
   };
 }
 
-export default function Home({ coffeeStores }) {
-  const { handleTrackLocation, latLong, locationErrorMsg, isFindingLocation } =
+export default function Home(props) {
+  const { handleTrackLocation, locationErrorMsg, isFindingLocation } =
     useTrackLocation();
+
+  const [coffeeStoresError, setCoffeeStoresError] = useState(null);
+
+  const { dispatch, state } = useContext(StoreContext);
+
+  const { coffeeStores, latLong } = state;
 
   useEffect(() => {
     async function fetchData() {
       if (latLong) {
         try {
-          const fetchedCoffeeStores = await fetchCoffeeStores(latLong);
-          console.log({ fetchedCoffeeStores });
+          const fetchedCoffeeStores = await fetchCoffeeStores(latLong, 30);
+          dispatch({
+            type: ACTION_TYPES.SET_COFFEE_STORES,
+            payload: {
+              coffeeStores: fetchedCoffeeStores,
+            },
+          });
         } catch (error) {
-          console.log({ error });
+          setCoffeeStoresError(error.message);
         }
       }
     }
     fetchData();
-  }, [latLong]);
+  }, [latLong, dispatch]);
 
   const HandleOnBannerBtnClick = () => {
     handleTrackLocation();
   };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -56,6 +69,7 @@ export default function Home({ coffeeStores }) {
           handleOnClick={HandleOnBannerBtnClick}
         />
         {locationErrorMsg && <p>Something went wrong: {locationErrorMsg}</p>}
+        {coffeeStoresError && <p>Something went wrong: {coffeeStoresError}</p>}
 
         <div className={styles.heroImage}>
           <Image
@@ -68,9 +82,27 @@ export default function Home({ coffeeStores }) {
 
         {coffeeStores.length > 0 && (
           <div className={styles.sectionWrapper}>
-            <h2 className={styles.heading2}>Guadalajara stores</h2>
+            <h2 className={styles.heading2}>Stores near me</h2>
             <div className={styles.cardLayout}>
               {coffeeStores.map(({ id, name, imgUrl, ...otherProps }) => (
+                <Card
+                  key={id}
+                  className={styles.card}
+                  name={name}
+                  imgUrl={imgUrl}
+                  href={`/coffee-store/${id}`}
+                  {...otherProps}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+
+        {props.coffeeStores.length > 0 && (
+          <div className={styles.sectionWrapper}>
+            <h2 className={styles.heading2}>Guadalajara stores</h2>
+            <div className={styles.cardLayout}>
+              {props.coffeeStores.map(({ id, name, imgUrl, ...otherProps }) => (
                 <Card
                   key={id}
                   className={styles.card}
